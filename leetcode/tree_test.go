d3d57c70f2d53640c0d7b5db9a892597b7eda005
jacky <jackyxiang1104@gmail.com>
2019-05-24 13:36:43 -0700

    1. [Tree] Add hasPathSum method. 112 https://leetcode.com/problems/path-sum/

--- leetcode/tree_test.go
+++ leetcode/tree_test.go
@@ -89,3 +89,17 @@ func maxValue(left, right int) int {
 	}
 	return right
 }
+
+func hasPathSum(root *TreeNode, sum int) bool {
+
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil && root.Val == sum {
+		return true
+	}
+
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+
+}
