a8babaa488eb610ee49484f376845fa233032aae
jacky <jackyxiang1104@gmail.com>
2019-05-31 09:12:13 -0700

    [Bit] 231. Power of Two https://leetcode.com/problems/power-of-two/

--- leetcode/bit_test.go
+++ leetcode/bit_test.go
@@ -115,3 +115,26 @@ func missingNumber2(nums []int) int {
 	}
 	return res * -1
 }
+
+/**
+231. Power of Two
+https://leetcode.com/problems/power-of-two/
+*/
+func isPowerOfTwo(n int) bool {
+
+	if n == 0 {
+		return false
+	}
+	if n == 1 {
+		return true
+	}
+	cnt := 0
+	for n > 0 {
+		if cnt > 1 {
+			return false
+		}
+		cnt += n & 1
+		n = n >> 1
+	}
+	return cnt == 1
+}
